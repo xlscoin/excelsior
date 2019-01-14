72d7638cdb0c1a637ac2bbef590666c9f1bb4df9
19morhpeus80 <morpheus@integrisol.online>
2019-01-14 09:15:34 +0000

    additional branding

--- src/config/Ascii.h
+++ src/config/Ascii.h
@@ -1,13 +1,38 @@
 // Copyright (c) 2018, The TurtleCoin Developers
 // Copyright (c) 2018, The DeroGold Association
-// Copyright (c) 2019, The Excelsior Developers
+// Copyright (c) 2019, The Excelsior Developer
 //
 // Please see the included LICENSE file for more information
 
 #pragma once
 
 const std::string asciiArt =
-"\n                                                                  \n"
+"\n                                                                    \n"
+"              <uuvvvvvvvvvvvvvvvvvnvnvnvnvnvnvnvnvnu>               \n"
+"              {1` -- --- ----- ---:::::::::::::::-=n(.              \n"
+"              {1;.::::::::::.:::::===;===;==;==;=:<nc               \n"
+"              {n;..:-:-.:-:.::-:.:;;:;;::;;:;;;::.=n(               \n"
+"              {1=sa,. :::-:.:.::--:;=;;;;;;;;::<ac|n(.              \n"
+"              {n: ?Qmwc..-.:::.._ayz=:;;:::_awm@!`=o(.              \n"
+"              {1;..-:V$Qga,: .<ZQQe:::::samQ#T!`:.=o(.              \n"
+"              {1; :-:+YSXWQmya,.dQk,=aymWBZZY+::;:<n(.              \n"
+"              {1;.::.. +?SXWWWQQWWQQQWW#ZZY^:.=:;:=n(.              \n"
+"              {n;.--:::..+?YXW$WQQQWW$#Y?+.:;;;;;:=nc               \n"
+"              {n; ::.:.::...--~>XQ2<+~-:::;;;;;:;-=o+               \n"
+"              =n>.--::-:-::::.._dQk;:;=;;;;;;;;;;:<S;               \n"
+"              :vi.-:.:.:-----..vQWWc.:;;;;;;;:;;::ve.               \n"
+"               <n;.-::-:-:.::.=vZUAn;:;;:;;;;;;;:<o>                \n"
+"                {a;.--:.:-::..)XXoXSc::;;;;:;;;:=v}                 \n"
+"                -3s..:-:-:.:..:v#Qmz::;;;;;;;;::ve`                 \n"
+"                 -{s..:-:.::-.:vZXQz::;;:;;;;:=ue`                  \n"
+"                  -{o,.::-:.:. 3ASZ1.;;;;;;;:=u}`                   \n"
+"                    +Sc..:.::-:-{m}-;;:;;;::a2^                     \n"
+"                     -So>.-:--..=*=:;;;;::i2!`                      \n"
+"                       -!o>. :::.:;;=;-:so!`                        \n"
+"                         -SSs,.-::;::=u2o`                          \n"
+"                            ~?ns,_=an?^                             \n"
+"                               ~!?!~                                \n"
+"                                                                    \n"
 "XXXXXXX.XX.  XX. XXXXXX.XXXXXXX.XX.     XXXXXXX.XX. XXXXXX. XXXXXX. \n"
 "XX/---- .XX.XX/ XX/---- XX/---- XX|     XX/---- XX|XX/---XX.XX/--XX.\n"
 "XXXXX.   .XXX/  XX|     XXXXX.  XX|     XXXXXXX.XX|XX|   XX|XXXXXX/ \n"
@@ -15,4 +40,3 @@ const std::string asciiArt =
 "XXXXXXX.XX/  XX..XXXXXX.XXXXXXX.XXXXXXX.XXXXXXX|XX|.XXXXXX/ XX|  XX|\n"
 ".------ .-   .-  .----- .------ .------ .------ .-  .-----  .-   .- \n"
 "=================Onward and upward to greater glory=================\n";
-
