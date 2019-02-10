5bed42919e60d8a704740cc1f4b6835deed18ef8
MaddestHatter <44808557+MaddestHatter@users.noreply.github.com>
2019-02-10 18:00:06 +0000

    [CORE] added interface for quering single transaction details

--- src/CryptoNoteCore/ICore.h
+++ src/CryptoNoteCore/ICore.h
@@ -69,6 +69,12 @@ public:
     std::unordered_set<Crypto::Hash> &transactionsUnknown) const = 0;
 
   virtual bool hasTransaction(const Crypto::Hash& transactionHash) const = 0;
+  /*!
+   * \brief getTransaction Queries a single transaction details blob from the chain or transaction pool
+   * \param hash The hash of the transaction
+   * \return The binary blob of the queried transaction, or none if the transaction does not exist.
+   */
+  virtual boost::optional<BinaryArray> getTransaction(const Crypto::Hash& hash) const = 0; 
   virtual void getTransactions(const std::vector<Crypto::Hash>& transactionHashes,
                                std::vector<BinaryArray>& transactions,
                                std::vector<Crypto::Hash>& missedHashes) const = 0;
