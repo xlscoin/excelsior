b44e329f9d25d07ca912687d3b3956bb3234a481
19morhpeus80 <morpheus@integrisol.online>
2019-03-01 23:40:18 +0000

    Must remember to comma seperate array elements

--- src/config/CryptoNoteConfig.h
+++ src/config/CryptoNoteConfig.h
@@ -118,7 +118,7 @@ static_assert(UPGRADE_VOTING_WINDOW > 1, "Bad UPGRADE_VOTING_WINDOW");
 /* Block heights we are going to have hard forks at */
 const uint64_t FORK_HEIGHTS[] =
 {
-    55000   // 0
+    55000,   // 0
     1000000 // 1
 };
 
